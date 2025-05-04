26692e6c1f91f23319475f26b4c942697b193f24
Sezer Seker <sekersezer.09@gmail.com>
2025-05-04 16:33:51 +0300

    #2

--- _BACKEND/backend/index.js
+++ _BACKEND/backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-// dosya isimlerini alıyor
+// dosya isimlerini alıyor.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
